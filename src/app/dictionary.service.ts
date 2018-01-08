1c190d45a17b847d86840362e089eca2c9fb6ab5
German Lashevich <german.lashevich@gmail.com>
2018-01-08 01:26:12 +0100

    ✨ Create and update methods for DictionaryService

--- src/app/dictionary.service.ts
+++ src/app/dictionary.service.ts
@@ -3,6 +3,13 @@ import { Dictionary } from './dictionary';
 import { DICTIONARIES } from './mock-dictionaries';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { switchMap } from 'rxjs/operators';
+
+function getMaxId(collection: Dictionary[]): number {
+  return collection
+    ? collection.reduce((prev, curr) => prev.id < curr.id ? curr : prev).id
+    : 0;
+}
 
 @Injectable()
 export class DictionaryService {
@@ -15,5 +22,20 @@ export class DictionaryService {
 
   getDictionary(id: number): Observable<Dictionary> {
     return of(DICTIONARIES.find(dictionary => dictionary.id === id));
+  }
+
+  createDictionary(dictionary: Dictionary): Observable<Dictionary> {
+    dictionary.id = getMaxId(DICTIONARIES) + 1;
+    DICTIONARIES.push(dictionary);
+    return of(dictionary);
+  }
+
+  updateDictionary(dictionary: Dictionary): Observable<Dictionary> {
+    return this.getDictionary(dictionary.id)
+      .pipe(switchMap((oldDictionary: Dictionary, i: number) => {
+        oldDictionary.name = dictionary.name;
+        oldDictionary.description = dictionary.description;
+        return of(oldDictionary);
+      }));
   }
 }
